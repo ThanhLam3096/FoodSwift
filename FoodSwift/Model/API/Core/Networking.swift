0e684d18582c44724f930772ea8419bd401375fb
Thanh Lâm <lethanhlam3096@gmail.com>
2024-10-16 19:42:02 +0700

    Update data for CollectionView

--- FoodSwift/Model/API/Core/Networking.swift
+++ FoodSwift/Model/API/Core/Networking.swift
@@ -6,3 +6,168 @@
 //
 
 import Foundation
+
+import Foundation
+import UIKit
+
+struct FeaturePartnesResult {
+    var featurePartnes: [Meal]
+}
+
+struct NationFoodResult {
+    var nationFood: [Meal]
+}
+
+//MARK: Enum
+enum APIResult<T> {
+    case failure(String)
+    case success(T)
+}
+
+//MARK: Typealias
+typealias APICompletion<T> = (APIResult<T>) -> Void
+
+final class Networking {
+    //MARK: - singleton
+    private static var sharedNetworking: Networking = {
+        let networking = Networking()
+        return networking
+    }()
+    
+    class func shared() -> Networking {
+        return sharedNetworking
+    }
+    
+    //MARK: - init
+    private init() { }
+    
+    //MARK: - request
+    func request(with urlString: String, completion: @escaping (Data?, APIError?) -> Void) {
+        guard let url = URL(string: urlString) else {
+            let error = APIError.error("URL lỗi")
+            completion(nil, error)
+            return
+        }
+        
+        let config = URLSessionConfiguration.ephemeral
+        config.waitsForConnectivity = true
+        
+        let session = URLSession(configuration: config)
+        let task = session.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                if let error = error {
+                    completion(nil, APIError.error(error.localizedDescription))
+                } else {
+                    if let data = data {
+                        completion(data, nil)
+                    } else {
+                        completion(nil, APIError.error("Data format is error."))
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    // MARK: - Public Functions Call Data
+    func getFeaturePartners(completion: @escaping APICompletion<FeaturePartnesResult>) {
+        guard let url = URL(string: Api.Path.apiFeaturedParners) else {
+            completion(.failure(App.String.alertFailedAPI))
+            return
+        }
+        let config = URLSessionConfiguration.ephemeral
+        config.waitsForConnectivity = true
+        let session = URLSession(configuration: config)
+        let task = session.dataTask(with: url) { (data, respone, error) in
+            DispatchQueue.main.async {
+                if let _ = error {
+                    completion(.failure(App.String.alertFailedToConnectAPI))
+                } else {
+                    do {
+                        // Parse dữ liệu JSON thành mảng Meal
+                        let listMealFeaturePartners = try JSONDecoder().decode([Meal].self, from: data!)
+                        let result = FeaturePartnesResult(featurePartnes: listMealFeaturePartners)
+                        completion(.success(result))
+                    } catch {
+                        completion(.failure(App.String.alertFailedToDataAPI))
+                    }
+                    //                    if let data = data, let arrayJson = data.toArrayJSON() {
+                    //                        for json in arrayJson {
+                    //                            let featurePartners = json["featurePartners"] as! [JSON]
+                    //                            var listMealFeaturePartners: [Meal] = []
+                    //                            for item in featurePartners {
+                    //                                let mealFeaturePartner = Meal(json: item)
+                    //                                listMealFeaturePartners.append(mealFeaturePartner)
+                    //                                let result = FeaturePartnesResult(featurePartnes: listMealFeaturePartners)
+                    //                                completion(.success(result))
+                    //                            }
+                    //                        }
+                    //                        completion(.success(FeaturePartnesResult(featurePartnes: [])))
+                    //                    }
+                    //                    else {
+                    //                        completion(.failure(App.String.alertFailedToDataAPI))
+                    //                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    // MARK: - Public Functions Call Data
+    func getFoodNation(completion: @escaping APICompletion<NationFoodResult>) {
+        guard let url = URL(string: Api.Path.apiNationFoodVietNam) else {
+            completion(.failure(App.String.alertFailedAPI))
+            return
+        }
+        let config = URLSessionConfiguration.ephemeral
+        config.waitsForConnectivity = true
+        let session = URLSession(configuration: config)
+        let task = session.dataTask(with: url) { (data, respone, error) in
+            DispatchQueue.main.async {
+                if let _ = error {
+                    completion(.failure(App.String.alertFailedToConnectAPI))
+                } else {
+                    if let data = data, let json = data.toJSON() {
+                        let listNationMeal = json["Meal"] as! [JSON]
+                        var nationMeal: [Meal] = []
+                        for item in listNationMeal {
+                            let mealFeaturePartner = Meal(json: item)
+                            nationMeal.append(mealFeaturePartner)
+                        }
+                        let result = NationFoodResult(nationFood: nationMeal)
+                        completion(.success(result))
+                    }
+                    else {
+                        completion(.failure(App.String.alertFailedToDataAPI))
+                    }                }
+            }
+        }
+        task.resume()
+    }
+    
+    //MARK: - downloader
+    func downloadImage(url: String, completion: @escaping (UIImage?) -> Void) {
+        guard let url = URL(string: url) else {
+            completion(nil)
+            return
+        }
+        let config = URLSessionConfiguration.default
+        config.waitsForConnectivity = true
+        let session = URLSession(configuration: config)
+        let task = session.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                if let _ = error {
+                    completion(nil)
+                } else {
+                    if let data = data {
+                        let image = UIImage(data: data)
+                        completion(image)
+                    } else {
+                        completion(nil)
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+}
--- FoodSwift/Views/HomeViewController/HomeViewVM.swift
+++ FoodSwift/Views/HomeViewController/HomeViewVM.swift
@@ -7,67 +7,125 @@
 
 import Foundation
 
+typealias Completion = (Bool, String) -> Void
+
 final class HomeViewVM {
     let listLocation: [String] = ["Ha Noi, Viet Nam", "New York, USA", "Parisn, France", "London, England", "Bac Kinh, China", "Tokyo, Japan", "Malina, India", "Kevin, Australia", "Moccow, Russia", "Munich ,Germany", "Madrid, Spanish", "Buenos Aires, Argentina", "Brasília, Brazil"]
     
     let listSliderFoodImage: [String] = ["food1", "food2", "food3", "food4", "food5"]
+    
+    var listMealFeaturePartners: [Meal] = []
+    var listNationFood: [Meal] = []
+    
+    // MARK: - Enum
+    enum CollectionType: Int {
+        case featurePartner = 0
+        case nationFood
+        case slider
+    }
+    
     // MARK: - TableView HeaderView Data
     func numberOfRowsInSectionHeaderView() -> Int {
         return listLocation.count
     }
-
+    
     func cellForRowAtHeaderView(indexPath: IndexPath) -> LocationListViewModel {
         let itemLocaiton = listLocation[indexPath.row]
         let model = LocationListViewModel(location: itemLocaiton)
         return model
     }
-
+    
     func heightForRowAtHeaderView() -> CGFloat {
         return 36
     }
     
-    // MARK: - CollectionView Slider Data
-    func numberOfRowsInSectionSlider() -> Int {
-        return listSliderFoodImage.count
+    // MARK: - CollectionView Featured Partners Data
+    func numberOfRowsInSectionCollectionView(type: CollectionType) -> Int {
+        switch type {
+        case .featurePartner:
+            return listMealFeaturePartners.count
+        case .nationFood:
+            return listNationFood.count
+        default:
+            return listSliderFoodImage.count
+        }
+        
     }
-
+    
+    func cellForRowAtCollectionMeal(indexPath: IndexPath, type: CollectionType) -> FeaturedPartnersCollectionViewCellViewModel {
+        switch type {
+        case .featurePartner:
+            let item = listMealFeaturePartners[indexPath.row]
+            let model = FeaturedPartnersCollectionViewCellViewModel(meal: item)
+            return model
+        default:
+            let item = listNationFood[indexPath.row]
+            let model = FeaturedPartnersCollectionViewCellViewModel(meal: item)
+            return model
+        }
+        
+    }
+    
     func cellForRowAtSlider(indexPath: IndexPath) -> SliderImageCollectionViewCellViewModel {
         let item = listSliderFoodImage[indexPath.row]
         let model = SliderImageCollectionViewCellViewModel(image: item)
         return model
     }
-
-    func heightForRowAtSlider() -> CGFloat {
-        return 36
-    }
     
-    // MARK: - CollectionView Featured Partners Data
-    func numberOfRowsInSectionFeaturedPartners() -> Int {
-        return dummyMealFeaturePartnes.featurePartnesMeal.count
-    }
-
-    func cellForRowAtFeaturedPartners(indexPath: IndexPath) -> FeaturedPartnersCollectionViewCellViewModel {
-        let item = dummyMealFeaturePartnes.featurePartnesMeal[indexPath.row]
-        let model = FeaturedPartnersCollectionViewCellViewModel(meal: item)
-        return model
-    }
-
-    func heightForRowAtFeaturedPartners() -> CGFloat {
-        return 254
+    func heightForRowAtCollectionView(type: CollectionType) -> CGFloat {
+        switch type {
+        case .slider:
+            return 36
+        default:
+            return 254
+        }
+        
     }
     
     // MARK: - TableView List Res Data
     func numberOfRowsInSectionListRestaurants() -> Int {
         return dummyRestaurant.listAllRes.count
     }
-
+    
     func cellForRowAtListRestaurants(indexPath: IndexPath) -> ListAllResTableViewCellVM {
         let item = dummyRestaurant.listAllRes[indexPath.row]
         let model = ListAllResTableViewCellVM(restaurant: item)
         return model
     }
-
+    
     func heightForRowAtListRestaurants() -> CGFloat {
         return 282
     }
+    
+    // MARK: - Public Function
+    func getAPIListFeaturePartners(listFeaturePartnersCompletion: @escaping (Bool, String) -> Void) {
+        Networking.shared().getFeaturePartners { [weak self] (mealResult) in
+            guard let this = self else { return }
+            switch mealResult {
+            case .failure(let error):
+                listFeaturePartnersCompletion(false, error)
+            case .success(let result):
+                for item in result.featurePartnes {
+                    this.listMealFeaturePartners.append(item)
+                }
+                listFeaturePartnersCompletion(true, App.String.loadSuccess)
+            }
+        }
+    }
+    
+    func getAPIListNationFood(listNationFoodCompletion: @escaping (Bool, String) -> Void) {
+        Networking.shared().getFoodNation { [weak self] (mealResult) in
+            guard let this = self else { return }
+            switch mealResult {
+            case .failure(let error):
+                listNationFoodCompletion(false, error)
+            case .success(let result):
+                for item in result.nationFood {
+                    this.listNationFood.append(item)
+                }
+                listNationFoodCompletion(true, App.String.loadSuccess)
+            }
+        }
+    }
+
 }
