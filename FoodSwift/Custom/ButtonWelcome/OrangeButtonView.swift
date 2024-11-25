1ad53f6336c1e4acbb8ada532655da05076c87a2
Thanh Lâm <lethanhlam3096@gmail.com>
2024-11-25 10:06:12 +0700

    Handle validate textField in Create Account Screen

--- FoodSwift/Custom/ButtonWelcome/OrangeButtonView.swift
+++ FoodSwift/Custom/ButtonWelcome/OrangeButtonView.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 import AVFoundation
+import AudioToolbox
 
 class OrangeButtonView: UIView {
 
     @IBOutlet private var orangeButtonView: UIView!
     @IBOutlet private weak var startButton: UIButton!
-    var delegate: OrangeButtonViewViewDelegate?
+    weak var delegate: OrangeButtonViewViewDelegate?
     
     //MARK: - Properties
     var audioPlayer: AVAudioPlayer?
@@ -25,11 +26,13 @@ class OrangeButtonView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUpUIView()
+//        preloadSound()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setUpUIView()
+//        preloadSound()
     }
     
     private func setUpUIView() {
@@ -45,7 +48,8 @@ class OrangeButtonView: UIView {
     }
     
     @IBAction func getStartButtonTouchUpInside(_ sender: Any) {
-        playSound()
+//        playSound()
+        playSoundWithSystemSound()
         if let delegate = delegate {
             delegate.tappingInsideButton(view: self)
         }
@@ -71,18 +75,32 @@ class OrangeButtonView: UIView {
 
     }
     
-    func playSound() {
+    private func preloadSound() {
         guard let url = Bundle.main.url(forResource: "ting_ting", withExtension: "mp3") else { return }
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: url)
-            audioPlayer?.play()
+            audioPlayer?.prepareToPlay()
         } catch let error {
             print("Error playing sound: \(error.localizedDescription)")
         }
     }
+    
+    private func playSound() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.audioPlayer?.play()
+        }
+    }
+    
+    private func playSoundWithSystemSound() {
+        if let soundURL = Bundle.main.url(forResource: "ting_ting", withExtension: "mp3") {
+            var soundID: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &soundID)
+            AudioServicesPlaySystemSound(soundID)
+        }
+    }
 }
 
-protocol OrangeButtonViewViewDelegate {
+protocol OrangeButtonViewViewDelegate: AnyObject {
     func tappingInsideButton(view: OrangeButtonView)
 }
